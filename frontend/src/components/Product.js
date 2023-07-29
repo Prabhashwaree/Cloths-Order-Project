21b10931100758ffd27fe06fa76b19f0d1f7c072
Prabhashearee <prabhashwaree57@gmail.com>
2023-07-29 15:07:39 +0530

    Update home page and user profile page

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -31,14 +31,15 @@ function Product(props) {
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
+        <img src={product.image} className="card-img-top" alt={product.name} 
+        style={{height:"300px"}}/>
       </Link>
       <Card.Body>
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>${product.price}</Card.Text>
+        <Card.Text><b>LKR .</b>{product.price}</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -7,6 +7,8 @@ import { toast } from 'react-toastify';
 import { getError } from '../utils';
 import axios from 'axios';
 
+import singinBackground from '../asset/sign in backgroud img.png';
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'UPDATE_REQUEST':
@@ -62,22 +64,29 @@ export default function ProfileScreen() {
   };
 
   return (
-    <div className="container small-container">
+    <div className="container small-container" 
+    style={{backgroundColor:"transparent",position:"relative",margin:"auto",textAlign:"center"}}>
+      
+      <img src={singinBackground} style={{position:"absolute",height:"88vh"}}/>
       <Helmet>
         <title>User Profile</title>
       </Helmet>
-      <h1 className="my-3">User Profile</h1>
+      <h1 className="my-3" 
+      style={{position:"relative",top:"38px",color:"gray"}}>User Profile</h1>
+      
       <form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Name</Form.Label>
+        <Form.Group className="mb-3" controlId="name" 
+        style={{width:"60%" ,position:"relative",margin:"auto",textAlign:"center",top:"22px"}} >
+          <Form.Label style={{position:"relative",right:"61%",top:"38px"}}><b>Name :</b></Form.Label>
           <Form.Control
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Email</Form.Label>
+        <Form.Group className="mb-3" controlId="name" 
+        style={{width:"60%" ,position:"relative",margin:"auto",textAlign:"center",top:"12px"}}>
+          <Form.Label style={{position:"relative",right:"61%",top:"38px"}}><b>Email :</b></Form.Label>
           <Form.Control
             type="email"
             value={email}
@@ -85,22 +94,25 @@ export default function ProfileScreen() {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
+        <Form.Group className="mb-3" controlId="password" 
+        style={{width:"60%" ,position:"relative",margin:"auto",textAlign:"center",bottom:"1px"}}>
+          <Form.Label style={{position:"relative",right:"65%",top:"38px"}}><b>Password :</b></Form.Label>
           <Form.Control
             type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Confirm Password</Form.Label>
+        <Form.Group className="mb-3" controlId="password" 
+        style={{width:"60%" ,position:"relative",margin:"auto",textAlign:"center",bottom:"15px"}}>
+          <Form.Label style={{position:"relative",right:"75%",top:"38px"}}><b>Confirm Password :</b></Form.Label>
           <Form.Control
             type="password"
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </Form.Group>
         <div className="mb-3">
-          <Button type="submit">Update</Button>
+          <Button type="submit" 
+          style={{position:"relative",top:"5px",width:"30%",backgroundColor:"green",color:"black"}}>Update</Button>
         </div>
       </form>
     </div>
